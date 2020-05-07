3a65852186006b07b6e3e350f8d336561fa13fe0
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-05-07 22:28:16 +0300

    Fix reducer (caused error on CoursesPage after we save some course)

--- src/redux/reducers/courceReducer.js
+++ src/redux/reducers/courceReducer.js
@@ -6,9 +6,9 @@ export default function courseReducer(state = initialState.courses, action) {
     case types.CREATE_COURSE_SUCCESS:
       return [...state, { ...action.course }];
     case types.UPDATE_COURSE_SUCCESS:
-      return state.map((course) => {
-        action.course.id === course.id ? action.course : course;
-      });
+      return state.map((course) =>
+        action.course.id === course.id ? action.course : course
+      );
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
     default:
