8f4a93e5cc1c6105f4a50d6229bd0e27b9d38159
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-05-08 22:02:12 +0300

    Introduce apiStatusActions and apiStatusReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import courses from "./courceReducer";
 import authors from "./authorReducer";
+import apiCallsInProgressReducer from "./apiStatusReducer";
 
 const rootReducer = combineReducers({
   courses,
   authors,
+  apiCallsInProgressReducer,
 });
 
 export default rootReducer;
